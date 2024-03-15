0f8dcbb983204424f76e952ff0484ce98b6d4758
Camilo Salazar <csalazar@jumpitt.com>
2024-03-15 13:32:43 -0300

    first commit with lazyvim configured

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,34 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      eslint = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            workspace = {
+              checkThirdParty = false,
+            },
+            codeLens = {
+              enable = true,
+            },
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      },
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
+}
---
+++ lua/plugins/vim-jsdoc.lua
@@ -0,0 +1,9 @@
+return {
+  "heavenshell/vim-jsdoc",
+  ft = "javascript,typescript,typescriptreact,svelte",
+  cmd = "JsDoc",
+  keys = {
+    { "<leader>jd", "<cmd>JsDoc<cr>", desc = "JsDoc" },
+  },
+  build = "make install",
+}
