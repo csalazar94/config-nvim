5d60f05fe52c42b7d862cd221d1556cbfcdd60ba
Camilo Salazar <camilosalazar94@gmail.com>
2024-04-25 20:21:34 -0400

    add maps for cycle buffers

--- lua/csalazar/plugins/bufferline.lua
+++ lua/csalazar/plugins/bufferline.lua
@@ -4,5 +4,7 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require("bufferline").setup{}
+    vim.keymap.set('n', "<S-h>", "<cmd>BufferLineCyclePrev<cr>")
+    vim.keymap.set('n', "<S-l>", "<cmd>BufferLineCycleNext<cr>")
   end
 }
