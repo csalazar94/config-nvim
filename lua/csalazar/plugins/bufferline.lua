c5612d563ccff20341c08cc16c2779b28129f84e
Camilo Salazar <camilosalazar94@gmail.com>
2024-04-25 22:08:28 -0400

    add buffer delete maps

--- lua/csalazar/plugins/bufferline.lua
+++ lua/csalazar/plugins/bufferline.lua
@@ -1,10 +1,12 @@
 return {
   'akinsho/bufferline.nvim',
-  version = "*", 
+  version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require("bufferline").setup{}
+    require("bufferline").setup {}
     vim.keymap.set('n', "<S-h>", "<cmd>BufferLineCyclePrev<cr>")
     vim.keymap.set('n', "<S-l>", "<cmd>BufferLineCycleNext<cr>")
+    vim.keymap.set('n', "<leader>bo", "<cmd>BufferLineCloseOthers<cr>")
+    vim.keymap.set('n', "<leader>bd", "<cmd>bd<cr>")
   end
 }
