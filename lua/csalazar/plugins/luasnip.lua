53c148013d97dfe78012e4e861d1702abba0043d
Camilo Salazar <camilosalazar94@gmail.com>
2024-05-22 09:53:52 -0400

    feat: add snippet for log with stringify

--- lua/csalazar/plugins/luasnip.lua
+++ lua/csalazar/plugins/luasnip.lua
@@ -6,9 +6,24 @@ return {
   build = "make install_jsregexp",
   dependencies = {
     {
-      "rafamadriz/friendly-snippets", 
+      "rafamadriz/friendly-snippets",
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
+        local ls = require("luasnip")
+        local extras = require("luasnip.extras")
+        local fmt = require("luasnip.extras.fmt").fmt
+        local s = ls.snippet
+        local i = ls.insert_node
+        local rep = extras.rep
+
+        ls.add_snippets("javascript", {
+          s("lo", fmt(
+            [[
+            console.log('{}', JSON.stringify({}, null, 2));
+            ]], {
+              i(1), rep(1)
+            }))
+        })
       end,
     },
   },
