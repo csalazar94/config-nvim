fe40debcbd728b76fb2c32249634fe6a63073ac8
Camilo Salazar <camilosalazar94@gmail.com>
2024-07-02 12:13:48 -0400

    feat: update lsp highlight autocmd

--- lua/csalazar/plugins/lsp.lua
+++ lua/csalazar/plugins/lsp.lua
@@ -74,14 +74,25 @@ return {
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.server_capabilities.documentHighlightProvider then
+          local highlight_augroup = vim.api.nvim_create_augroup('csalazar-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
+            group = highlight_augroup,
             callback = vim.lsp.buf.document_highlight,
           })
 
           vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
             buffer = event.buf,
+            group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('csalazar-lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = 'csalazar-lsp-highlight', buffer = event2.buf }
+            end,
           })
         end
       end,
