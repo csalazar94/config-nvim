833c4664ba57b3bb3a373cedaf9c9bf629f67cab
Camilo Salazar <camilosalazar94@gmail.com>
2024-07-03 14:34:00 -0400

    feat: update dashboard configuration

--- lua/csalazar/plugins/dashboard.lua
+++ lua/csalazar/plugins/dashboard.lua
@@ -1,8 +1,45 @@
+local logo = [[
+ ██████╗███████╗ █████╗ ██╗      █████╗ ███████╗ █████╗ ██████╗
+██╔════╝██╔════╝██╔══██╗██║     ██╔══██╗╚══███╔╝██╔══██╗██╔══██╗
+██║     ███████╗███████║██║     ███████║  ███╔╝ ███████║██████╔╝
+██║     ╚════██║██╔══██║██║     ██╔══██║ ███╔╝  ██╔══██║██╔══██╗
+╚██████╗███████║██║  ██║███████╗██║  ██║███████╗██║  ██║██║  ██║
+ ╚═════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
+]]
+
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
+      shuffle_letter = false,
+      config = {
+        header = vim.split(logo, "\n"),
+        shortcut = {
+          { icon = '󰚰 ',
+            desc = 'Sync',
+            group = '@property',
+            action = 'Lazy sync',
+            key = 's',
+          },
+          {
+            icon = ' ',
+            desc = 'Files',
+            group = '@variable',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            icon = '󰿅 ',
+            desc = 'Quit',
+            group = 'Label',
+            action = function() vim.api.nvim_input("<cmd>qa<cr>") end,
+            key = 'q',
+          },
+        },
+        project = { enable = false },
+        mru = { limit = 10, cwd_only = true },
+      },
     }
   end,
   dependencies = { { 'nvim-tree/nvim-web-devicons' } }
