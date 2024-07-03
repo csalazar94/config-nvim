0749df31ced0f1115052f83244710c6b21060cbf
Camilo Salazar <camilosalazar94@gmail.com>
2024-07-03 12:50:49 -0400

    feat: add ts-context-comment

--- lua/csalazar/plugins/minicomment.lua
+++ lua/csalazar/plugins/minicomment.lua
@@ -1,6 +1,9 @@
 return {
   "echasnovski/mini.comment",
-  config = function()
-    require('mini.comment').setup()
-  end
+  opts = {
+    custom_commentstring = function()
+      return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
+    end,
+  },
+  config = true
 }
