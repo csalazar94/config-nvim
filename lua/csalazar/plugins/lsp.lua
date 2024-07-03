1daffabedfa91979b0d6a7bdd0f400a501199541
Camilo Salazar <camilosalazar94@gmail.com>
2024-07-03 12:49:23 -0400

    feat: add neodev and illuminate

--- lua/csalazar/plugins/lsp.lua
+++ lua/csalazar/plugins/lsp.lua
@@ -1,11 +1,25 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
+    "folke/neodev.nvim",
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
+    require("neodev").setup({
+      -- add any options here, or leave empty to use the default settings
+    })
+    local lspconfig = require('lspconfig')
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace"
+          }
+        }
+      }
+    })
     require("mason").setup()
     local cmp_lsp = require("cmp_nvim_lsp")
     local capabilities = vim.tbl_deep_extend(
@@ -23,12 +37,12 @@ return {
       },
       handlers = {
         function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
+          lspconfig[server_name].setup {
             capabilities = capabilities
           }
         end,
         ["tsserver"] = function()
-          require("lspconfig")["tsserver"].setup {
+          lspconfig["tsserver"].setup {
             capabilities = capabilities,
             init_options = {
               preferences = {
@@ -66,35 +80,6 @@ return {
         map('<leader>ca', vim.lsp.buf.code_action)
         map('K', vim.lsp.buf.hover)
         map('gD', vim.lsp.buf.declaration)
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-          local highlight_augroup = vim.api.nvim_create_augroup('csalazar-lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
-
-          vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('csalazar-lsp-detach', { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'csalazar-lsp-highlight', buffer = event2.buf }
-            end,
-          })
-        end
       end,
     })
   end
