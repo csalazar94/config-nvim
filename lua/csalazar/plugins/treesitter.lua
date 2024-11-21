fcc248130a0661ee8103f619b48c006453d5ae0e
Camilo Salazar <camilosalazar94@gmail.com>
2024-11-21 11:00:02 -0300

    feat: add kulala and markdown preview

--- lua/csalazar/plugins/treesitter.lua
+++ lua/csalazar/plugins/treesitter.lua
@@ -2,13 +2,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require 'nvim-treesitter.configs'.setup {
       -- A list of parser names, or "all" (the five listed parsers should always be installed)
-      ensure_installed = { 
+      ensure_installed = {
         "vimdoc", "javascript", "typescript", "lua", "rust",
         "jsdoc", "bash", "markdown", "markdown_inline", "html",
         "json", "jsonc", "python", "tsx", "astro", "css", "go",
-        "gomod", "gowork", "gosum", "toml", "prisma",
+        "gomod", "gowork", "gosum", "toml", "prisma", "http", "graphql"
       },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
